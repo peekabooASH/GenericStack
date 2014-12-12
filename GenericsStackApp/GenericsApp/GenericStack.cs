def4dc729756e1b9935964b728c3747dcc87bbf2
peekabooASH <maloncho04@gmail.com>
2014-12-13 00:51:54 +0600

    first commit

---
+++ GenericsStackApp/GenericsApp/GenericStack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsApp
+{
+    public class GenericStack<T>
+    {
+
+        public int i = 0;
+        public T[] items;
+        public GenericStack(int size)
+        {
+            items = new T[size + 1];
+        }
+        public void Push(T item)
+        {
+            this.items[i++] = item;
+
+        }
+
+        public T Pop()
+        {
+            T a = items[--i];
+            return (a);
+        }
+    }
+}
---
+++ GenericsStackApp/GenericsApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericStack<int> items = new GenericStack<int>(5);
+            Console.WriteLine("Int type Stack:");
+            items.Push(11);
+            items.Push(12);
+            items.Push(13);
+            items.Push(14);
+            Console.WriteLine(items.Pop());
+            Console.WriteLine(items.Pop());
+            Console.WriteLine(items.Pop());
+            Console.WriteLine(items.Pop());
+            GenericStack<string> itemss = new GenericStack<string>(5);
+            Console.WriteLine("String type Stack:");
+            itemss.Push("Ash");
+            itemss.Push("Maloncho");
+            itemss.Push("Ayesha");
+            itemss.Push("Siddiqua");
+            Console.WriteLine(itemss.Pop());
+            Console.WriteLine(itemss.Pop());
+            Console.WriteLine(itemss.Pop());
+            Console.WriteLine(itemss.Pop());
+            Console.ReadKey();
+        }
+    }
+}
